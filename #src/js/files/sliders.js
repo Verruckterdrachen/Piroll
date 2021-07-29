a30e55215ea216667c1b414a979c7d93b3bcfac6
verruckterdrachen <verruckterdrachen@gmail.com>
2021-07-29 14:56:31 +0300

    feat: add testimonials slider

--- #src/js/files/sliders.js
+++ #src/js/files/sliders.js
@@ -61,63 +61,19 @@ if (sliderScrollItems.length > 0) {
 
 function sliders_bild_callback(params) { }
 
-let slider_about = new Swiper('.about__slider', {
-	/*
-	effect: 'fade',
-	autoplay: {
-		delay: 3000,
-		disableOnInteraction: false,
-	},
-	*/
-	observer: true,
-	observeParents: true,
-	slidesPerView: 1,
-	spaceBetween: 0,
-	autoHeight: true,
-	speed: 800,
-	//touchRatio: 0,
-	//simulateTouch: false,
-	//loop: true,
-	//preloadImages: false,
-	//lazy: true,
-	// Dotts
-	//pagination: {
-	//	el: '.slider-quality__pagging',
-	//	clickable: true,
-	//},
-	// Arrows
-	navigation: {
-		nextEl: '.about__more .more__item_next',
-		prevEl: '.about__more .more__item_prev',
-	},
-	/*
-	breakpoints: {
-		320: {
-			slidesPerView: 1,
-			spaceBetween: 0,
-			autoHeight: true,
-		},
-		768: {
-			slidesPerView: 2,
-			spaceBetween: 20,
-		},
-		992: {
-			slidesPerView: 3,
-			spaceBetween: 20,
-		},
-		1268: {
-			slidesPerView: 4,
-			spaceBetween: 30,
-		},
-	},
-	*/
-	on: {
-		lazyImageReady: function () {
-			ibg();
-		},
-	}
-	// And if we need scrollbar
-	//scrollbar: {
-	//	el: '.swiper-scrollbar',
-	//},
-});
+if (document.querySelector('.slider-testimonials__body')) {
+  let testimonialsSlider = new Swiper('.slider-testimonials__body', {
+    observer: true,
+    observeParents: true,
+    slidesPerView: 1,
+    autoHeight: false,
+    speed: 1200,
+    parallax: true,
+    loop: true,
+    pagination: {
+    	el: '.slider-testimonials__dotts',
+    	clickable: true,
+    }
+  });
+}
+
